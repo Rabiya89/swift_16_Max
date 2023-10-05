c008eb48ce43500ec7637ba0207023c625a02e8a
Рабия Ланге <rabiya_lange@MacBook-Pro-Rabia.local>
2023-10-05 22:24:24 +0300

    First commit Max

---
+++ swift_16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_16
+//
+//  Created by Рабия Ланге on 05.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
